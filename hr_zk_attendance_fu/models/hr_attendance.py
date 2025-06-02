04ab9366fec350b74652854c153f33df81f9eb43
SP10 <paredes.seba@gmail.com>
2025-06-02 18:44:03 -0300

    Add files via upload
    Actualizacion controles varios edificios

--- hr_zk_attendance_fu/models/hr_attendance.py
+++ hr_zk_attendance_fu/models/hr_attendance.py
@@ -4,3 +4,9 @@ class HrAttendance(models.Model):
     _inherit = 'hr.attendance'
 
     address_id = fields.Many2one('res.partner', string='Work Location')
+    zk_machine_id = fields.Many2one(
+        'zk.machine',
+        string='Biometric Device',
+        help='Biometric device from which the attendance was downloaded',
+        readonly=True
+    )
