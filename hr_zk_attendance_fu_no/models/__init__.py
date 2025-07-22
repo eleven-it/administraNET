9d7aa1b1a3efde9d89974bac201c78e55a6581ff
Sebastian Paredes <sparedes@administranet.com.ar>
2025-07-22 13:25:11 +0200

    Commit automático de cambios locales antes de push

--- hr_zk_attendance_fu_no/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import hr_attendance
-from . import machine_analysis
-from . import zk_machine_inherit
-from . import building
-from . import zk_machine
-from . import hr_attendance_inconsistency
--- hr_zk_attendance_fu_no/models/machine_analysis.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class ZkMachineAttendanceExtension(models.Model):
-    _inherit = 'zk.machine.attendance'
-
-    attendance_type = fields.Selection(selection_add=[
-        ('16', 'Desconocido')
-    ])
